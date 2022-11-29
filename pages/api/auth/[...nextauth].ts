e91ed24fa8fccb2510f7b1e2efcc9d3c71118a07
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2022-11-29 11:11:47 +0700

    fix: debug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,27 +1,88 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import { JWT } from "next-auth/jwt";
-
 import KeycloakProvider from "next-auth/providers/keycloak";
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
+async function refreshAccessToken(token: JWT) {
+  try {
+    if (Date.now() > token.refreshTokenExpired) throw Error;
+    const details = {
+      client_id: process.env.NX_NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
+      client_secret: process.env.NX_NEXT_PUBLIC_KEYCLOAK_CLIENT_SECRET,
+      grant_type: ["refresh_token"],
+      refresh_token: token.refreshToken,
+    };
+    const formBody: string[] = [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Object.entries(details).forEach(([key, value]: [string, any]) => {
+      const encodedKey = encodeURIComponent(key);
+      const encodedValue = encodeURIComponent(value);
+      formBody.push(encodedKey + "=" + encodedValue);
+    });
+    const formData = formBody.join("&");
+    const url = `${process.env.NX_NEXT_PUBLIC_KEYCLOAK_BASE_URL}/token`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: formData,
+    });
 
-export const authOptions: NextAuthOptions = {
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+      refreshTokenExpired:
+        Date.now() + refreshedTokens.refresh_expires_in * 1000,
+    };
+  } catch (error) {
+    console.log("error", error);
+    let ErrMessage;
+    switch (error.error) {
+      case "invalid_grant":
+        ErrMessage = "SessionNotActive";
+        break;
+
+      default:
+        ErrMessage = "RefreshAccessTokenError";
+        break;
+    }
+    return {
+      ...token,
+      error: ErrMessage,
+    };
+  }
+}
+
+export const authOptions = {
   providers: [
     KeycloakProvider({
-      clientId: "fitness-app",
-      clientSecret: "SmeYEExCsyBLDcKfoAKVEg7VXnzB8vpe",
-      issuer: "https://lab-ids.savvycom.xyz/realms/savvy-fitness",
+      clientId: `${process.env.NX_NEXT_PUBLIC_KEYCLOAK_CLIENT_ID}`,
+      clientSecret: `${process.env.NX_NEXT_PUBLIC_KEYCLOAK_CLIENT_SECRET}`,
+      issuer: `${process.env.NX_NEXT_PUBLIC_KEYCLOAK_ISSUER}`,
       idToken: true,
     }),
   ],
-  callbacks: {
-    async jwt(data) {
-      return data.token;
-      // console.log({ token, user, account, profile });
 
-      // // Initial sign in
+  callbacks: {
+    async jwt({ token, user, account, profile }) {
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Initial sign in
       // if (account && user && profile) {
+      //   user.familyName = profile.family_name;
+      //   user.givenName = profile.given_name;
+      //   user.emailVerified = profile.email_verified;
       //   return {
       //     accessToken: account.access_token,
       //     accessTokenExpires: account.expires_at * 1000,
@@ -31,23 +92,42 @@ export const authOptions: NextAuthOptions = {
       //   };
       // }
 
-      // // Return previous token if the access token has not expired yet
-      // if (Date.now() < token.accessTokenExpires) {
-      //   console.log(token);
-      //   return token;
-      // }
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
 
-      // // Access token has expired, try to update it
-      // return refreshAccessToken(token);
+      // Access token has expired, try to update it
+      return refreshAccessToken(token);
     },
-    async session({ session, token }: { session: any; token: any }) {
+    async session({ session, token }) {
       if (token) {
         // Return all data into user
-        session.token = token;
+        session.user = { ...token.user, ...token.account, ...token.profile };
+        session.accessToken = token.accessToken;
+        session.error = token.error;
       }
       return session;
     },
+
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) return url;
+      // split from callbackUrl {'signOut?idToken'}
+      const urlSplit = url.split("?");
+      const idToken = urlSplit[1];
+      // This is the beef:
+      if (urlSplit[0] === "signOut") {
+        const ssoLogoutUrl = `${process.env.NX_NEXT_PUBLIC_KEYCLOAK_BASE_URL}/logout`;
+        const redirectUrl = process.env.NEXTAUTH_URL;
+        const signOutWithRedirectUrl = `${ssoLogoutUrl}?post_logout_redirect_uri=${encodeURIComponent(
+          redirectUrl
+        )}&id_token_hint=${idToken}`;
+        return signOutWithRedirectUrl;
+      }
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return new URL(url, baseUrl).toString();
+      return baseUrl;
+    },
   },
 };
-
 export default NextAuth(authOptions);
